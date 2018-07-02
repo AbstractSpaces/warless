04cc2fdbdceabecff98d726cc041c86d0cc4192a
Dylan Johnson <dj.bytheriver@gmail.com>
2018-07-02 16:13:40 +0800

    v0.1 - Wrote Entity class. First real code!

---
+++ Server/src/game/Entity.ts
@@ -0,0 +1,45 @@
+﻿import { normalise } from "./Vector";
+
+export abstract class Entity {
+    // Not yet sure how much time I should spend restricting access to instance variables.
+    public hp: number;
+
+    constructor(
+        readonly MAX_HP: number,
+        readonly SPEED: number,
+        readonly W: number,
+        readonly H: number,
+        public team: number,
+        public x: number,
+        public y: number,
+        public dX: number,
+        public dY: number,
+        public r: number,
+    ) {
+        this.hp = this.MAX_HP;
+    }
+
+    public accelerate(x: number, y: number): void {
+        [this.dX, this.dY] = normalise(this.dX + x, this.dY + y, this.SPEED);
+    }
+
+    public stop(): void {
+        [this.dX, this.dY] = [0, 0];
+    }
+
+    public turn(r: number): void {
+        this.r = r;
+    }
+
+    // In the current version, all collisions have the same effect.
+    public collide(): void {
+        this.stop();
+        // TODO: walk back if sprites are overlapping.
+    }
+
+    public cut(): void {
+        this.hp -= 1;
+    }
+
+    public abstract die(): void;
+}
---
+++ Server/src/game/Vector.ts
@@ -0,0 +1,9 @@
+﻿// 2D vector calculations.
+
+// Set magnitude for vector (x,y) = mag.
+export function normalise(x: number, y: number, mag: number): number[] {
+    // Don't want to divide by 0.
+    if (x == 0 && y == 0) return [0, 0];
+    const old: number = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    return [x / old * mag, y / old * mag];
+}
